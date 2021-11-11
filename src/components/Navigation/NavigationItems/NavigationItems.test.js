df13048401748879446309301157bed29cd4f157
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-11-11 15:22:27 +0530

    use helper method beforeEach

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,14 +7,19 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 configure({ adapter: new Adapter() }); //connect enzyme to app
 
 describe("<NavigationItems />", () => {
+    let wrapper;
+    beforeEach(()=>{
+     wrapper = shallow(<NavigationItems />);
+    })
   // 2 items burgerbuilder, auth
   it("should render two <NavigationItem /> elements if not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
+   
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
   // 3 items burgerbuilder, orders, logout
   it("should render three <NavigationItem /> elements if authenticated", () => {
-    const wrapper = shallow(<NavigationItems isAuthenticated />);
+   // wrapper = shallow(<NavigationItems isAuthenticated />); or
+   wrapper.setProps({isAuthenticated: true});
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
